738b67fe1a8f1e210b3ac687179e2c0cb31be40c
ArthurWANGZHE <100007307+ArthurWANGZHE@users.noreply.github.com>
2024-01-28 09:28:10 +0800

    fix

--- test_v2.py
+++ test_v2.py
@@ -10,7 +10,6 @@ import cv2
 import math
 import matplotlib.pyplot as plt
 
-
 sys.path.append("/opt/MVS/Samples/64/Python/MvImport")
 from MvCameraControl_class import *
 
@@ -22,6 +21,7 @@ class HikCamera:
     SerialIndex = ''  # 相机序列号
     IsConnect = 0  # 相机是否连接
     nPayloadSize = -1
+
     # cam = MvCamera()
 
     # CALL_BACK_FUN = EventInfoCallBack(event_callback)
@@ -157,7 +157,7 @@ class HikCamera:
         # 发送软件触发命令
         ret = self.cam.MV_CC_SetCommandValue("TriggerSoftware")
         if ret != 0:
-            rospy.loginfo("Set command value failed! ret[0x%x]" % ret)
+            print("Set command value failed! ret[0x%x]" % ret)
             return 0, 0
 
         stFrameInfo = MV_FRAME_OUT_INFO_EX()
@@ -171,7 +171,10 @@ class HikCamera:
             temp = np.frombuffer(pData, dtype=np.uint8)
             x = np.ascontiguousarray(temp).reshape(-1).astype(np.uint8)
             HImage = ha.gen_image1('byte', stFrameInfo.nWidth, stFrameInfo.nHeight, x.ctypes.data)
-            return 1, HImage
+            temp = np.asarray(pData)  # 将c_ubyte_Array转化成ndarray得到（3686400，）
+            temp = temp.reshape(3072, 2048, 1)
+            gray = cv2.cvtColor(temp, cv2.COLOR_BGR2RGB)
+            return 1, gray
         del pData
         rospy.loginfo("Grab image failed!")
         return 0, 0
@@ -201,76 +204,35 @@ class HikCamera:
         self.DisConnect()
 
 
+def undistort_brown_single_pt(pt, kappa, max_iter=10):
+    x_distorted, y_distorted = pt
+    r_distorted_sq = x_distorted ** 2 + y_distorted ** 2
+
+    # 初始估计
+    x_undistorted, y_undistorted = pt
+
+    for _ in range(max_iter):
+        r_undistorted_sq = x_undistorted ** 2 + y_undistorted ** 2
+        distortion = 1 + kappa * r_undistorted_sq
+        x_undistorted = x_distorted / distortion
+        y_undistorted = y_distorted / distortion
+
+    return np.array([x_undistorted, y_undistorted])
+
+
+def undistort_brown(pts, kappa, max_iter=10):
+    return np.array([undistort_brown_single_pt(pt, kappa, max_iter) for pt in pts])
+
+
 class FindBox:
     # 类内变量
     CameraHandle = HikCamera("")  # 相机句柄
-    leftModelID = ha.hhandle  # 二维码模板
-    rightModelID = ha.hhandle  # 二维码模板
     BarCodeHandle = ha.hhandle  # 一维码模板
     leftRefPose = [1, 1, 1, 1, 1, 1]  # 参考位姿
     rightRefPose = [1, 1, 1, 1, 1, 1]  # 参考位姿
     FilePath = ''
 
-    # 初始化
-    # 应该用一个相机参数文件(boxpileup.config)初始化本类
-    # 每台相机参数单独文件夹保存
-    # 参数：
-    #   file_name:相机参数文件(boxpileup.config)的绝对路径
     def __init__(self, left_file_name, right_file_name):
-        # 由于机器人小叉不平整，左右两侧料箱需要分别标定，连接相机和cad及保存图片（放在左侧标定内）文件公用一份
-        # 报错问题未标注,部分错误由halcon自带的提示,但初始化出错属于严重问题
-
-        # 读取相机参数文件，该文件应该包括：
-        # 相机序列号，应该利用halcon软件尝试连接相机，读取其序列号(不是海康sdk软件显示的序列号)
-        # 相机参数文件名 (绝对路径)
-        # 相机位姿文件名 (绝对路径)
-        # 二维码cad文件名 (绝对路径)
-        # 正确摆放料箱位姿文件名(绝对路径)
-        # 照片保存文件夹 (绝对路径),本参数目前不使用，为预留参数
-        left_file = open(left_file_name, 'r')  # 打开左侧标定文件
-        right_file = open(right_file_name, 'r')  # 打开右侧标定文件
-        left_file_data = left_file.readlines()  # 读取所有行
-        right_file_data = right_file.readlines()  # 读取所有行
-
-        # 头两行为预留说明
-        self.camIndex = left_file_data[2].replace('\n', '')
-        leftCamParaPath = left_file_data[3].replace('\n', '')
-        leftCamPosePath = left_file_data[4].replace('\n', '')
-        rightCamParaPath = right_file_data[3].replace('\n', '')
-        rightCamPosePath = right_file_data[4].replace('\n', '')
-        cadPath = left_file_data[5].replace('\n', '')
-        leftRefPosePath = left_file_data[6].replace('\n', '')
-        rightRefPosePath = right_file_data[6].replace('\n', '')
-        self.FilePath = left_file_data[7].replace('\n', '')
-
-        # 读取相机参数和相机位姿
-        leftCamParam = ha.read_cam_par(leftCamParaPath)
-        leftCamPose = ha.read_pose(leftCamPosePath)
-        rightCamParam = ha.read_cam_par(rightCamParaPath)
-        rightCamPose = ha.read_pose(rightCamPosePath)
-        # 读取二维码模板
-        Contours, DxfStatus = ha.read_contour_xld_dxf(cadPath, [], [])
-        HomMat2DIdentity = ha.hom_mat2d_identity()
-        HomMat2DScale = ha.hom_mat2d_scale(HomMat2DIdentity, 0.0001, 0.0001, 0.5, 0.5)
-        ContoursTrans = ha.affine_trans_contour_xld(Contours, HomMat2DScale)
-
-        # 建立二维码模板
-        self.leftModelID = ha.create_planar_calib_deformable_model_xld(ContoursTrans, leftCamParam, leftCamPose, 'auto',
-                                                                       [], [],
-                                                                       'auto', 0.9, [], 'auto', 0.9, [], 'auto', 'none',
-                                                                       'ignore_local_polarity', 5, [], [])
-        self.rightModelID = ha.create_planar_calib_deformable_model_xld(ContoursTrans, rightCamParam, rightCamPose,
-                                                                        'auto', [], [],
-                                                                        'auto', 0.9, [], 'auto', 0.9, [], 'auto',
-                                                                        'none',
-                                                                        'ignore_local_polarity', 5, [], [])
-        # 建立一维码识别库
-        self.BarCodeHandle = ha.create_bar_code_model([], [])
-        ha.set_bar_code_param(self.BarCodeHandle, 'element_size_min', 1.5)
-
-        # 读取正确拍照参考位姿
-        self.leftRefPose = ha.read_pose(leftRefPosePath)
-        self.rightRefPose = ha.read_pose(rightRefPosePath)
 
     # 拍照二次定位
     # 参数：
@@ -278,16 +240,17 @@ class FindBox:
     # Return:
     #   1,dx,dy,dz,rad,DecodedDataStrings[0]
     #   是否成功，dx，dy，dz(单位：mm)，rad(单位:°)，一维码code(string)
+
     def LocateAndRecong(self, IsAlwaysSaveImage, LeftOrRight):
         # 拍照
         rospy.loginfo(f"@#$ {datetime.datetime.now()}, Begin LocateAndRecong")
-        tempIndex, Image = self.CameraHandle.CaptureImage()
+        tempIndex, gray = self.CameraHandle.CaptureImage()
         retry_times = 0
         while retry_times < 3 and tempIndex == 0:
             rospy.loginfo(f"{datetime.datetime.now()}, Capture Image failed.Try again. retry_times: {retry_times}")
             self.CameraHandle = HikCamera(self.camIndex)
             self.CameraHandle.Connect()
-            tempIndex, Image = self.CameraHandle.CaptureImage()
+            tempIndex, gray = self.CameraHandle.CaptureImage()
             retry_times += 1
         if tempIndex == 0:
             # 严重问题
@@ -296,15 +259,15 @@ class FindBox:
 
         # 查找二维码定位
         MIN_MATCH_COUNT = 10
-        template = cv2.imread('template_.png', 0)
+        template = cv2.imread('/home/zhang/my_hocol/src/ssr_pkg/scripts/template_.png', 0)
 
         # Create a pyramid for the template image
         template_pyr = [template]
         for i in range(3):
             template_pyr.append(cv2.pyrDown(template_pyr[i]))
 
-        target = cv2.imread('20240124163906.jpeg', 0)
-        target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
+        target = gray
+        target = cv2.normalize(target, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
 
         # Create a pyramid for the target image
         target_pyr = [target]
@@ -361,15 +324,14 @@ class FindBox:
                            matchesMask=matchesMask,
                            flags=2)
 
-#        result = cv2.drawMatches(template, kp1, target, kp2, good, None, **draw_params)
-#        plt.imshow(result, 'gray')
-#        plt.show()
+        #        result = cv2.drawMatches(template, kp1, target, kp2, good, None, **draw_params)
+        #        plt.imshow(result, 'gray')
+        #        plt.show()
 
         x1, y1 = dst[0][0][0], dst[0][0][1]
         x2, y2 = dst[1][0][0], dst[1][0][1]
         x3, y3 = dst[2][0][0], dst[2][0][1]
         x4, y4 = dst[3][0][0], dst[3][0][1]
-
 
         # 相机参数
         fx, fy, cx, cy = 0.0158218, 0.0158218, 1426.27, 1072.45
@@ -386,38 +348,20 @@ class FindBox:
         # 0.0545613, 0.0319774, 0.214913]
         t = np.array([0.0545613, 0.0319774, 0.214913])
         kappa = -179.202
-
-        # 去畸变函数
-        def undistort_brown_single_pt(pt, kappa, max_iter=10):
-            x_distorted, y_distorted = pt
-            r_distorted_sq = x_distorted ** 2 + y_distorted ** 2
-
-            # 初始估计
-            x_undistorted, y_undistorted = pt
-
-            for _ in range(max_iter):
-                r_undistorted_sq = x_undistorted ** 2 + y_undistorted ** 2
-                distortion = 1 + kappa * r_undistorted_sq
-                x_undistorted = x_distorted / distortion
-                y_undistorted = y_distorted / distortion
-
-            return np.array([x_undistorted, y_undistorted])
-
-        def undistort_brown(pts, kappa, max_iter=10):
-            return np.array([undistort_brown_single_pt(pt, kappa, max_iter) for pt in pts])
+        K = np.array([[fx, 0, cx],
+                      [0, fy, cy],
+                      [0, 0, 1]])
 
         # 相机内参
         K = np.array([[fx, 0, cx],
                       [0, fy, cy],
                       [0, 0, 1]])
 
-
         # 三联二维码矩形的四个角点的像素坐标
         qr_corners = np.array([[x1, y1],
                                [x2, y2],
                                [x3, y3],
                                [x4, y4]], dtype='float32')
-
 
         # 计算三联二维码矩形的中心点的像素坐标
         qr_center = np.mean(qr_corners, axis=0)
@@ -443,7 +387,7 @@ class FindBox:
         rad = math.atan2(R[2][1], R[2][2]) * 180 / math.pi
         print(dx, dy, dz, rad)
 
-
+        """
         # 一维码识别，DecodedDataStrings是string类型
         SymbolRegions, DecodedDataStrings = ha.find_bar_code(Image, self.BarCodeHandle, 'Code 128')
 
@@ -472,10 +416,10 @@ class FindBox:
             other_StyleTime = now.strftime("%Y%m%d%H%M%S")
             rospy.loginfo(self.FilePath + other_StyleTime + 'jpeg. OK')
             ha.write_image(Image, 'jpeg', 0, self.FilePath + other_StyleTime + '.jpeg')
-
+        """
         # 返回执行成功和参数
-        rospy.loginfo(f"@#$ {datetime.datetime.now()}, dx, dy, dz, rad, DecodedDataStrings[0]: {dx, dy, dz, rad, DecodedDataStrings[0] if DecodedDataStrings else 0}")
-        return 1, dx, dy, dz, rad, DecodedDataStrings[0] if DecodedDataStrings else 0
+        rospy.loginfo(f"@#$ {datetime.datetime.now()}, dx, dy, dz, rad: {dx, dy, dz, rad}")
+        return 1, dx, dy, dz, rad  # DecodedDataStrings[0] if DecodedDataStrings else 0
 
     def __del__(self):
         # 软件关闭前或软件重启前等应该尝试关闭相机
